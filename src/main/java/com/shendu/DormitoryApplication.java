483bc348ae21543e7afa799bf4862b5cd07ac7da
TJK <1752105743@qq.com>
2025-03-31 23:45:16 +0800

    宿舍管理系统“
    ”

---
+++ src/main/java/com/shendu/DormitoryApplication.java
@@ -0,0 +1,10 @@
+package com.shendu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryApplication {
+    public static void main(String[] args) {
+
+    }
+}
